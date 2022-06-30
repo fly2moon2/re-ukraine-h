2e387ee3a7ef93cae77d2064c2e40bee4202dfc9
fly2moon2 <fly2moon2@yahoo.com>
2022-06-30 21:31:12 +0800

    userlist

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,26 +1,41 @@
-import {useState} from 'react';
+import {useState,Fragment} from 'react';
 import datafile from "../../static.json";
 const {users} = datafile;
 //import {users} from "../../static.json";
 
 export default function UsersList () {
   const [userIndex, setUserIndex] = useState(0);
+  const user = users[userIndex];
 
   return (
-    <ul className="users items-list-nav">
-      {users.map((u, i) => (
-        <li
-          key={u.id}
-          className={i === userIndex ? "selected" : null}
-        >
-          <button
-            className="btn"
-            onClick={() => setUserIndex(i)}
+    <Fragment>
+      <ul className="users items-list-nav">
+        {users.map((u, i) => (
+          <li
+            key={u.id}
+            className={i === userIndex ? "selected" : null}
           >
-            {u.name}
-          </button>
-        </li>
-      ))}
-    </ul>
+            <button
+              className="btn"
+              onClick={() => setUserIndex(i)}
+            >
+              {u.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      {user && (
+        <div className="item user">
+          <div className="item-header">
+            <h2>{user.name}</h2>
+          </div>
+          <div className="user-details">
+            <h3>{user.title}</h3>
+            <p>{user.notes}</p>
+          </div>
+        </div>
+      )}
+    </Fragment>
   );
 }
